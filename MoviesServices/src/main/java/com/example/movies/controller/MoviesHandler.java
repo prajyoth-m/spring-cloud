91c29ae8fef7eac628c63907208f12129d75460e
prajyoth-m <m.prajyoth@live.com>
2022-03-06 20:18:21 +0530

    modifying insert movie API to get data from OMDB database

--- MoviesServices/src/main/java/com/example/movies/controller/MoviesHandler.java
+++ MoviesServices/src/main/java/com/example/movies/controller/MoviesHandler.java
@@ -8,19 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.movies.bean.BaseResponse;
 import com.example.movies.bean.Movies;
 import com.example.movies.repository.MoviesRepository;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @RestController
 public class MoviesHandler {
 
 	@Autowired
 	MoviesRepository moviesRepo;
-
+	
 	@GetMapping("/movies/{name}")
 	public Movies moviesByName(@PathVariable String name) {
 		return moviesRepo.findById(name).orElse(null);
@@ -31,10 +33,11 @@ public class MoviesHandler {
 		return StreamSupport.stream(moviesRepo.findAll().spliterator(), false).collect(Collectors.toList());
 	}
 
-	@PostMapping("/movie")
-	public BaseResponse insertMovie(@RequestBody Movies movie) {
+	@PostMapping("/movie/{movie}")
+	public BaseResponse insertMovie(@PathVariable String movie) {
 		BaseResponse resp = new BaseResponse();
-		Movies savedMovie = moviesRepo.save(movie);
+		Movies imdbMovies = getMovieFromIMDB(movie);
+		Movies savedMovie = moviesRepo.save(imdbMovies);
 		if (null != savedMovie) {
 			resp.setSuccess(true);
 			resp.setName(savedMovie.getName());
@@ -42,6 +45,21 @@ public class MoviesHandler {
 			resp.setSuccess(false);
 		}
 		return resp;
+	}
+	
+	private Movies getMovieFromIMDB(String query) {
+		String URL = "http://www.omdbapi.com/?apikey=bcff06d7&t="+query;
+		RestTemplate restTemplate = new RestTemplate();
+		String responseIMDB = restTemplate.getForEntity(URL, String.class).getBody();
+		JsonObject json = new Gson().fromJson(responseIMDB, JsonObject.class);
+		Movies movie = new Movies();
+		movie.setDescription(json.get("Plot").getAsString());
+		movie.setName(json.get("Title").getAsString());
+		movie.setDirector(json.get("Director").getAsString());
+		movie.setGenre(json.get("Genre").getAsString());
+		movie.setImage(json.get("Poster").getAsString());
+		movie.setRating(json.get("imdbRating").getAsFloat());
+		return movie;
 	}
 
 }
