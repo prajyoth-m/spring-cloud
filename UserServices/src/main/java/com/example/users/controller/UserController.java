8c039e16d8e59c21e1d37b34d1426f2a416f7efb
prajyoth-m <m.prajyoth@live.com>
2022-03-06 20:21:02 +0530

    adding more API's
    - Delete users API
    - Get user by department API
    - Insert/Update user API enhancement

--- UserServices/src/main/java/com/example/users/controller/UserController.java
+++ UserServices/src/main/java/com/example/users/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.users.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +25,11 @@ public class UserController {
 	public Users getUserByName(@PathVariable String username) {
 		return userRepo.findById(username).orElse(null);
 	}
+	
+	@GetMapping("/users/{department}")
+	public List<Users> getUsersByDept(@PathVariable String department){
+		return userRepo.findByDepartment(department);
+	}
 
 	@PostMapping("/user/validate")
 	public ValidateUser validateUser(@RequestBody Users userData) {
@@ -30,6 +38,8 @@ public class UserController {
 		valid.setUsername(userData.getUsername());
 		if (user != null) {
 			valid.setPasswordAccepted(userData.getPassword().equals(user.getPassword()));
+			valid.setDepartment(user.getDepartment());
+			valid.setUsername(user.getUsername());
 		} else {
 			valid.setPasswordAccepted(Boolean.FALSE);
 		}
@@ -46,7 +56,16 @@ public class UserController {
 		} else {
 			resp.setSuccess(false);
 		}
-		return null;
+		return resp;
 	}
-
+	
+	@DeleteMapping("/user/{username}")
+	public BaseResponse deleteUser(@PathVariable String username) {
+		BaseResponse resp = new BaseResponse();
+		userRepo.deleteById(username);
+		resp.setSuccess(true);
+		resp.setUsername(username);
+		return resp; 
+	}
+	
 }
--- UserServices/src/main/java/com/example/users/repository/UserRepository.java
+++ UserServices/src/main/java/com/example/users/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.users.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.example.users.beans.Users;
 
 @Repository
 public interface UserRepository extends CrudRepository<Users, String> {
+	
+	List<Users> findByDepartment(String department);
 }
